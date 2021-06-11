4e43cafaf20a03ec7bf61533c528df9163d3496c
agozdz <aleksandra.gozdz@currenda.pl>
2021-06-11 13:51:18 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace altkomAleksandraGozdz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine("Hello World!");
+            for(int i = 0; i < 10; i++) {
+                Console.WriteLine(random.Next(100, 999));
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
